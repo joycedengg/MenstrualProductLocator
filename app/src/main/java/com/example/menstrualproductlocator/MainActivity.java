247bf1829369c7b3945bdfed266a989a67c6d14d
Joyce Deng <joycedeng@fb.com>
2021-08-03 11:02:23 -0700

    Added AnimatedBottomBar

--- app/src/main/java/com/example/menstrualproductlocator/MainActivity.java
+++ app/src/main/java/com/example/menstrualproductlocator/MainActivity.java
@@ -15,9 +15,14 @@ import com.example.menstrualproductlocator.fragments.MapsFragment;
 import com.example.menstrualproductlocator.fragments.ProfileFragment;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import nl.joery.animatedbottombar.AnimatedBottomBar;
+
 public class MainActivity extends AppCompatActivity {
 
-    private BottomNavigationView bottomNavigationView;
+    private AnimatedBottomBar bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,13 +30,18 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         final FragmentManager fragmentManager = getSupportFragmentManager();
-        bottomNavigationView = findViewById(R.id.bottomNavigation);
 
-        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+        bottomNavigationView = findViewById(R.id.bottom_bar);
+        bottomNavigationView.setTabEnabledById(R.id.action_map, true);
+        bottomNavigationView.setTabEnabledById(R.id.action_chat, true);
+        bottomNavigationView.setTabEnabledById(R.id.action_profile, true);
+
+
+        bottomNavigationView.setOnTabSelectListener(new AnimatedBottomBar.OnTabSelectListener() {
             @Override
-            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            public void onTabSelected(int lastIndex, @Nullable AnimatedBottomBar.Tab lastTab, int newIndex, @NotNull AnimatedBottomBar.Tab newTab) {
                 Fragment fragment = null;
-                switch (item.getItemId()) {
+                switch (newTab.getId()) {
                     case R.id.action_map:
                         fragment = new MapsFragment();
                         break;
@@ -45,10 +55,38 @@ public class MainActivity extends AppCompatActivity {
                         break;
                 }
                 fragmentManager.beginTransaction().replace(R.id.flContainer, fragment).commit();
-                return true;
+            }
+
+            @Override
+            public void onTabReselected(int i, @NotNull AnimatedBottomBar.Tab tab) {
+
             }
         });
-        bottomNavigationView.setSelectedItemId(R.id.action_map);
+
+        bottomNavigationView.selectTabById(R.id.action_map, true);
+
+//        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+//            @Override
+//            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+//                Fragment fragment = null;
+//                switch (item.getItemId()) {
+//                    case R.id.action_map:
+//                        fragment = new MapsFragment();
+//                        break;
+//                    case R.id.action_chat:
+//                        fragment = new ChatFragment();
+//                        break;
+//                    case R.id.action_profile:
+//                        fragment = new ProfileFragment();
+//                        break;
+//                    default:
+//                        break;
+//                }
+//                fragmentManager.beginTransaction().replace(R.id.flContainer, fragment).commit();
+//                return true;
+//            }
+//        });
+//        bottomNavigationView.setSelectedItemId(R.id.action_map);
 
     }
 }
